61b161e18b2d1cea6815658c0165da39b802e866
mandu543@gmail.com <mandu543@gmail.com>
2026-03-11 13:18:46 +0000

    incremental bookings OK + add comment to change default value schema project

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,3 +1,4 @@
+{# permet de changer le schema mis dans par defaut dans le projet #}
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
--- models/00_bronze/bronze_bookings.sql
+++ models/00_bronze/bronze_bookings.sql
@@ -1,1 +1,8 @@
-select * from {{ source('staging_schema','bookings') }}
+{% set incremental_flag = 1 %}
+{% set incremental_col = 'created_at' %}
+
+select * from {{ source('staging_schema','bookings') }} 
+
+{% if incremental_flag == 1 %}
+    WHERE {{ incremental_col }} > (SELECT COALESCE(MAX({{ incremental_col }}), '1900-01-01') FROM {{ this }})
+{% endif %}
