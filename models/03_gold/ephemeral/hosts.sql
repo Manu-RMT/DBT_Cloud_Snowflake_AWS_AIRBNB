9c7811030ffa7311ad7d83e9e1d86efdbfbbd457
mandu543@gmail.com <mandu543@gmail.com>
2026-03-13 15:15:35 +0000

    ephemeral hosts ok + snapshot OK pour l'insertion

---
+++ models/03_gold/ephemeral/hosts.sql
@@ -0,0 +1,13 @@
+WITH hosts as (
+    SELECT
+        HOST_ID,
+        HOST_NAME,
+        HOST_SINCE,
+        IS_SUPERHOST,
+        RESPONSE_RATE_QUALITY,
+        HOST_CREATED_AT
+    FROM 
+        {{ ref('obt') }}
+)
+
+select * from hosts
--- models/03_gold/obt.sql
+++ models/03_gold/obt.sql
@@ -15,7 +15,7 @@
         },
         {
             "table" : "AIRBNB_DB.SILVER.SILVER_HOSTS",
-            "columns" : "SILVER_hosts.HOST_NAME, SILVER_hosts.HOST_SINCE, SILVER_hosts.RESPONSE_RATE, SILVER_hosts.RESPONSE_RATE_QUALITY, SILVER_hosts.CREATED_AT AS HOST_CREATED_AT",
+            "columns" : "SILVER_hosts.HOST_NAME, SILVER_hosts.HOST_SINCE, SILVER_hosts.IS_SUPERHOST, SILVER_hosts.RESPONSE_RATE, SILVER_hosts.RESPONSE_RATE_QUALITY, SILVER_hosts.CREATED_AT AS HOST_CREATED_AT",
             "alias" : "SILVER_hosts",
             "join_condition" : "SILVER_listings.HOST_ID = SILVER_hosts.HOST_ID"
         }
