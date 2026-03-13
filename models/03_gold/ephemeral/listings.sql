ea43ab6208eab7c52a5b33d0fc2fd58196f2be60
mandu543@gmail.com <mandu543@gmail.com>
2026-03-13 15:43:20 +0000

    DIM Airbnb Listing OK + mise en place snapshot (inconvenient debug ephemeral)

---
+++ models/03_gold/ephemeral/listings.sql
@@ -0,0 +1,18 @@
+WITH listings as (
+    SELECT
+        LISTING_ID,
+        PROPERTY_TYPE,
+        ROOM_TYPE,
+        CITY,
+        COUNTRY,
+        ACCOMMODATES,
+        BEDROOMS,
+        BATHROOMS,
+        PRICE_PER_NIGHT_TAG,
+        LISTING_CREATED_AT
+    FROM
+        {{ ref('obt') }}
+
+)
+
+SELECT * FROM listings
