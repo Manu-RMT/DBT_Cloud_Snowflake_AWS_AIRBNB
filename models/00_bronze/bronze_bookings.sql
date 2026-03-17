540dba49ad73e927f8aa79dad239aedbd174fb6f
Manuel RAMANITRA <mandu543@gmail.com>
2026-03-17 13:25:06 +0000

    en cas de doublon, on en garde qu'un dans le bronze

--- models/00_bronze/bronze_bookings.sql
+++ models/00_bronze/bronze_bookings.sql
@@ -1,7 +1,26 @@
-{{ config(materialize='incremental') }}
+{{ config(materialize='incremental', unique_key='BOOKING_ID') }}
 
-select * from {{ source('staging_schema','bookings') }} 
+WITH cleaned AS (
+   SELECT *,
+          ROW_NUMBER() OVER (PARTITION BY BOOKING_ID ORDER BY CREATED_AT DESC) AS rn
+   FROM {{ source('staging_schema','bookings') }}
+)
+
+SELECT 
+    BOOKING_ID,
+    LISTING_ID,
+    BOOKING_DATE,
+    NIGHTS_BOOKED,
+    BOOKING_AMOUNT,
+    CLEANING_FEE,
+    SERVICE_FEE,
+    BOOKING_STATUS,
+    CREATED_AT
+FROM cleaned
+WHERE rn = 1
 
 {% if is_incremental() %}
-    WHERE CREATED_AT > (SELECT COALESCE(MAX(CREATED_AT), '1900-01-01') FROM {{ this }})
+   AND CREATED_AT > (SELECT COALESCE(MAX(CREATED_AT), '1900-01-01') FROM {{ this }})
 {% endif %}
+
+ 
